509081b2b1c329a8505600ded56a4fe8ac9db0e5
jayson7 <lexxiijoo70@gmail.com>
2022-02-26 00:50:34 +0100

    open webcam with python

---
+++ main.py
@@ -0,0 +1,12 @@
+import calendar 
+
+year = 2022 
+month = 2 
+
+# if you want to make year and month an input from the user then cancel out line 3 and 4 and un-comment the ffg
+
+# year = int(input('Enter Year : '))
+# month = int(input('Enter Month : '))
+
+# display the calendar 
+print(calendar.month(year, month))
